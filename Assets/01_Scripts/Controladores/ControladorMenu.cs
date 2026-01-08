3e8cb6087ae2914d40c46107abe18d1546f75c7a
DamJuan <148950672+DamJuan@users.noreply.github.com>
2026-01-08 16:11:38 +0100

    mejora menu

--- Assets/01_Scripts/Controladores/ControladorMenu.cs
+++ Assets/01_Scripts/Controladores/ControladorMenu.cs
@@ -1,9 +1,60 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ControladorMenu : MonoBehaviour
 {
+    [Header("UI General")]
     public GameObject panelOpciones;
+    public Slider sliderVolumen;
+    public Toggle togglePantalla;
+
+    [Header("Fondos")]
+    public GameObject[] listaDeFondos;
+
+    void Start()
+    {
+        ElegirFondoAleatorio();
+
+        float volumenGuardado = PlayerPrefs.GetFloat("volumenGuardado", 1f);
+        AudioListener.volume = volumenGuardado;
+        if (sliderVolumen != null)
+            sliderVolumen.value = volumenGuardado;
+
+        bool esPantallaCompleta = PlayerPrefs.GetInt("pantallaCompleta", 1) == 1;
+        Screen.fullScreen = esPantallaCompleta;
+        if (togglePantalla != null)
+            togglePantalla.isOn = esPantallaCompleta;
+    }
+
+
+    public void CambiarVolumen(float valor)
+    {
+        AudioListener.volume = valor;
+        PlayerPrefs.SetFloat("volumenGuardado", valor);
+    }
+
+    public void ActivarPantallaCompleta(bool esCompleta)
+    {
+        Screen.fullScreen = esCompleta;
+        PlayerPrefs.SetInt("pantallaCompleta", esCompleta ? 1 : 0);
+    }
+
+
+    void ElegirFondoAleatorio()
+    {
+        if (listaDeFondos.Length == 0) return;
+
+        foreach (GameObject fondo in listaDeFondos)
+        {
+            if (fondo != null) fondo.SetActive(false);
+        }
+
+        int indiceAleatorio = Random.Range(0, listaDeFondos.Length);
+
+        if (listaDeFondos[indiceAleatorio] != null)
+            listaDeFondos[indiceAleatorio].SetActive(true);
+    }
 
     public void Jugar()
     {
