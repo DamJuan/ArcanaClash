57c24ca9d76987a99d7085857ed5bf0aefbb8dde
DamJuan <148950672+DamJuan@users.noreply.github.com>
2026-01-09 12:28:39 +0100

    mejoras

---
+++ Assets/01_Scripts/Controladores/ControladorJuego.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class ControladorJuego : MonoBehaviour
+{
+    public GameObject panelPausa;
+    private bool estaPausado = false;
+
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (estaPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    public void Pausar()
+    {
+        panelPausa.SetActive(true);
+        Time.timeScale = 0f;
+        estaPausado = true;
+    }
+
+    public void Reanudar()
+    {
+        panelPausa.SetActive(false);
+        Time.timeScale = 1f;
+        estaPausado = false;
+    }
+
+    public void IrAlMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+}
